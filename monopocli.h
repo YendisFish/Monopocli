37b3adb11c408f003f30e21009a4e5c78431a5c8
YendisFish <brintonsid@gmail.com>
2022-04-10 13:57:58 -0600

    staged

--- monopocli.h
+++ monopocli.h
@@ -1,36 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
-
-#ifndef PLAYER_H
-#define PLAYER_H
+#ifndef MONOPOCLI_H
+#define MONOPOCLI_H
 
 struct Player{
     int Money;
     int Location;
 };
 
-#endif
-
-#ifndef PROPERTY_H
-#define PROPERTY_H
+struct Board{
+    struct Property *properties;
+    int players;
+    struct Player *playerlist;
+};
 
 struct Property{
     char name[100];
     int cost;
     int rent;
     struct Player *owner;
-};
-
-#endif
-
-#ifndef BOARD_H
-#define BOARD_H
-
-struct Board{
-    struct Property *properties;
-    int players;
-    struct Player *playerlist;
 };
 
 #endif
